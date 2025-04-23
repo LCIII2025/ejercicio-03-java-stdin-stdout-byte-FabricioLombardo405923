3082f6ee31e5ed2066d6a5252342a0272382cd06
FABRICIO LOMBARDO <405923@tecnicatura.frc.utn.edu.ar>
2025-04-23 20:41:25 -0300

    resuelto

--- src/main/java/ar/edu/utn/frc/tup/lciii/App.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/App.java
@@ -11,13 +11,16 @@ public class App
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
+        //System.out.printf("Inserte un Dato Byte: ");
         byte a = scan.nextByte();
-        // Complete this line
-        // Complete this line
+       // System.out.printf("El segundo Byte: ");
+        byte a2 = scan.nextByte();
+       // System.out.printf("El Tercer Byte: ");
+        byte a3 = scan.nextByte();
 
         System.out.println(a);
-        // Complete this line
-        // Complete this line
+        System.out.println(a2);
+        System.out.println(a3);
 
     }
 }
